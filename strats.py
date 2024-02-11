0f339237805a9a99d2576ededa3da96a040b09fc
trentsteel777 <trentsteel777@gmail.com>
2024-02-11 16:57:46 +0000

    added in calc_discounted_saving to discount savings rate

--- strats.py
+++ strats.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from opt_gen import put, call
 from opt import Option, calc_short_put_profit_loss, calc_short_call_profit_loss
 from util import round_to_nearest_5, third_friday_of_next_month
@@ -91,6 +93,60 @@ class TradingData:
         self.stochslowk = stochslowk
         self.stochslowd = stochslowd
 
+#https://www.macrotrends.net/countries/USA/united-states/inflation-rate-cpi#google_vignette
+inflation_data = {
+    1990: 5.398,
+    1991: 4.235,
+    1992: 3.0288,
+    1993: 2.9517,
+    1994: 2.6074,
+    1995: 2.8054,
+    1996: 2.9312,
+    1997: 2.3377,
+    1998: 1.5523,
+    1999: 2.188,
+    2000: 3.3769,
+    2001: 2.8262,
+    2002: 1.586,
+    2003: 2.2701,
+    2004: 2.6772,
+    2005: 3.3927,
+    2006: 3.2259,
+    2007: 2.8527,
+    2008: 3.8391,
+    2009: -0.3555,
+    2010: 1.64,
+    2011: 3.1568,
+    2012: 2.0693,
+    2013: 1.4648,
+    2014: 1.6222,
+    2015: 0.1186,
+    2016: 1.2616,
+    2017: 2.1301,
+    2018: 2.4426,
+    2019: 1.8122,
+    2020: 1.2336,
+    2021: 4.6979,
+    2022: 8.0028,
+    2023: 8.0028,
+    2024: 8.0028,
+}
+
+
+def calc_discounted_saving(td):
+    future_value = 1000                       # Future value (amount to be discounted)
+    years = date.today().year - td.today.year # Number of years
+    r = inflation_data[td.today.year] / 100   
+    discount_rate = r                         # Average annual inflation rate
+
+    present_value = future_value / (1 + discount_rate) ** years
+
+    print("Present value after", years, "years:", round(present_value, 2))
+
+    return present_value
+
+
+
 class SSaveThousandPerMonth:
     def __init__(self):
         self.cash=1000
@@ -98,7 +154,7 @@ class SSaveThousandPerMonth:
 
     def run(self, td):
         if td.today.month != self.last_month_purchased:
-            self.cash+=1000
+            self.cash+=calc_discounted_saving(td)
             self.last_month_purchased=td.today.month
 
     def profit_loss(self):
@@ -114,7 +170,7 @@ class SDollarCostAveraging:
     def run(self, td):
         self.last_close = td.close
         if td.today.month != self.last_month_purchased:
-            self.cash+=1000
+            self.cash+=calc_discounted_saving(td)
             self.last_month_purchased=td.today.month
             self.buy(td)
 
